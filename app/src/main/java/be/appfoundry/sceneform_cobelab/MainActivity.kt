9b940eb1d7f687212d13866437c5d54f9f31d413
Mustafa Alroomi <mustafa.alroomi@appfoundry.be>
2019-06-25 13:13:46 +0200

    add init files

---
+++ app/src/main/java/be/appfoundry/sceneform_cobelab/MainActivity.kt
@@ -0,0 +1,12 @@
+package be.appfoundry.sceneform_cobelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
